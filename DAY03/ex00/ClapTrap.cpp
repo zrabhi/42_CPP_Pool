fcda0fa551fc0f7608037936b5b0146cff4be17a
zakaria <zakaria@zakarias-MacBook-Pro.local>
2022-11-05 20:12:09 +0100

    working on the next module (03)

---
+++ DAY03/ex00/ClapTrap.cpp
@@ -0,0 +1,37 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+    std::cout << "Constructor Called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string& _name) 
+    : a_damage(0), e_point(10), h_point(10), name(_name) 
+{
+    std::cout << " Copy Constructor Called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Destructor Called" << std::endl;    
+}
+
+ClapTrap& ClapTrap::operator = (const ClapTrap& obj)
+{
+    std::cout << "Copy assignement operator called" << std::endl;
+    if (this != &obj)
+        this->name = obj.name;
+    return(*this);
+}   
+
+void    ClapTrap::attack( const std::string& target)
+{
+
+
+}
+
+void    ClapTrap::takeDamage( unsigned int amount )
+{
+
+}
+
---
+++ DAY03/ex00/ClapTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+
+
+#include <iostream>
+
+class ClapTrap {
+    private:
+        std::string  name;
+        unsigned int h_point;
+        unsigned int e_point;
+        unsigned int a_damage;
+    public:
+        ClapTrap();
+        ClapTrap(std::string& _name);
+        ClapTrap& operator = ( const ClapTrap& obj );
+        ~ClapTrap();
+        
+        void    attack( const std::string& target);
+        void    takeDamage( unsigned int amount );
+        void    beRepaired( unsigned int amount );
+
+
+
+} ;
+
+
