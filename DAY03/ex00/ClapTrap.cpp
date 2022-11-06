e498ad44fcc2de828178097f806af94b8d78c7e7
ZAKARIA RABHI <zrabhi@student.1337.ma>
2022-11-06 02:16:50 +0100

    almost finishing the first one, just some few things left to check

--- DAY03/ex00/ClapTrap.cpp
+++ DAY03/ex00/ClapTrap.cpp
@@ -1,3 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zrabhi <zrabhi@student.1337.ma >           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 01:39:59 by zrabhi            #+#    #+#             */
+/*   Updated: 2022/11/06 02:12:20 by zrabhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap()
@@ -5,10 +18,16 @@ ClapTrap::ClapTrap()
     std::cout << "Constructor Called" << std::endl;
 }
 
-ClapTrap::ClapTrap(std::string& _name) 
-    : a_damage(0), e_point(10), h_point(10), name(_name) 
+ClapTrap::ClapTrap(const ClapTrap& obj)
+    :name(obj.name), h_point(obj.h_point), e_point(obj.e_point), a_damage(obj.a_damage)
 {
-    std::cout << " Copy Constructor Called" << std::endl;
+    std::cout << "Copy Constructor Called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string _name) 
+    : name(_name), h_point(10), e_point(10), a_damage(0)
+{
+    std::cout << "Copy name Constructor Called" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
@@ -19,19 +38,49 @@ ClapTrap::~ClapTrap()
 ClapTrap& ClapTrap::operator = (const ClapTrap& obj)
 {
     std::cout << "Copy assignement operator called" << std::endl;
+    
     if (this != &obj)
+    {
         this->name = obj.name;
-    return(*this);
+        this->h_point = obj.h_point;
+        this->e_point = obj.e_point;
+        this->a_damage = obj.a_damage;
+    }
+    return (*this);
 }   
 
 void    ClapTrap::attack( const std::string& target)
 {
 
+    std::cout << "ClapTrap ";
+    if (this->e_point && this->h_point)
+    {
+        std::cout << this->name + " attacks " + target + " causing " << this->h_point << " points of damage" << std::endl;
+        this->h_point = 0;
+        this->e_point--;
+    }
+    else
+        std::cout << "can't do anything without HitsPoints and EnergyPoints" << std::endl;
+}
 
+
+void    ClapTrap::beRepaired( unsigned int amount )
+{
+    if (amount > 0)
+    {
+        if ( this->e_point )
+        { 
+            this->h_point = amount;
+            this->e_point--;
+        }      
+        else
+            std::cout << " Sorry, ClapTrap can't do anything without EnergyPoints" << std::endl;
+    }
+    
 }
 
 void    ClapTrap::takeDamage( unsigned int amount )
 {
-
+    this->h_point -= amount;
+    
 }
-
--- DAY03/ex00/ClapTrap.hpp
+++ DAY03/ex00/ClapTrap.hpp
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zrabhi <zrabhi@student.1337.ma >           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 01:39:55 by zrabhi            #+#    #+#             */
+/*   Updated: 2022/11/06 01:47:34 by zrabhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
 #ifndef CLAPTRAP_H
 # define CLAPTRAP_H
 
@@ -8,21 +22,20 @@
 class ClapTrap {
     private:
         std::string  name;
-        unsigned int h_point;
+        unsigned int h_point; //----represent the health of the ClapTrap.
         unsigned int e_point;
         unsigned int a_damage;
     public:
         ClapTrap();
-        ClapTrap(std::string& _name);
+        ClapTrap( const ClapTrap& obj );
+        ClapTrap( std::string _name );
         ClapTrap& operator = ( const ClapTrap& obj );
         ~ClapTrap();
         
         void    attack( const std::string& target);
         void    takeDamage( unsigned int amount );
         void    beRepaired( unsigned int amount );
-
-
-
 } ;
 
 
+#endif
